475a1af0257c3a9ac2f6105756b9627567fe410b
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-24 23:10:03 +0100

    Prepare outlets and actions

---
+++ Randomizer/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Randomizer
+//
+//  Created by Maxim Makarov on 24.11.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getRandomNumberButton.layer.cornerRadius = 10
+    }
+
+    @IBAction func getRandomNumberButtonTapped() {
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
+        
+        randomValueLabel.text = Int.random(in: minimumNumber...maximumNumber).formatted()
+    }
+    
+
+}
+
