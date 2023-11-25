da94e378fbe079df2e09e43e40c317055b94de45
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-26 00:25:50 +0100

    Create and prepare SettingsViewController

---
+++ Randomizer/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  Randomizer
+//
+//  Created by Maxim Makarov on 26.11.2023.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
