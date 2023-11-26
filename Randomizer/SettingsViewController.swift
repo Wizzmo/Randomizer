cbac98d03d6a071071d85c23a1c51252cb6eeca0
Maxim Makarov <makarovmaxqa@gmail.com>
2023-11-26 22:51:59 +0100

    Create outlets and action for SettingsViewController

--- Randomizer/SettingsViewController.swift
+++ Randomizer/SettingsViewController.swift
@@ -9,7 +9,15 @@ import UIKit
 
 final class SettingsViewController: UIViewController {
 
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func cancelButtonAction() {
+        dismiss(animated: true)
+    }
+    
 }
